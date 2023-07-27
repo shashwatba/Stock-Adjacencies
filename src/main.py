298cf71d90fa1a3d6c782892d7254fb030891a19
christosskoun <christosskoun@gmail.com>
2023-07-27 09:58:27 -0400

    Added a comment of basic similarity score algorithm

--- src/main.py
+++ src/main.py
@@ -4,17 +4,28 @@ import pandas as pd
 import yfinance as yahooFinance
 
 '''
-Option 1:
-Correlation Graph: You can model correlations between different stocks. 
-In this graph, an edge between two stocks might indicate a strong correlation (either positive or negative) 
-between their price movements. If the correlation exceeds a certain threshold, you could add an edge in the graph. 
-This graph would enable you to visualize clusters of highly correlated stocks.
+Let's start by defining a similarity score that ranges from 0 to 1, with 1 being the most similar:
 
+Similarity Score = Industry Similarity Score * 0.5 + Market Cap Similarity Score * 0.3 + Dividend Similarity Score * 0.2
 
+Each individual score also ranges from 0 to 1. Here's how you might calculate each one:
 
-Option 2:
-Sector/Industry Graph: You could model a graph where edges represent stocks being in the same sector or industry. 
-This can help you visualize how different sectors/industries are interconnected based on shared companies.
+Industry Similarity Score: This one's simple. If the two stocks are in the same industry, the score is 1. If not, the score is 0.
+
+Market Cap Similarity Score: First, you might define what counts as a "similar" market cap. For instance, you might say 
+that two stocks are similar if their market caps are within 10% of each other. To calculate the score:
+
+If the market caps are within 20% of each other, the score is 1.
+If one stock's market cap is more than double the other's, the score is 0.
+For anything in between, you could calculate the score as 1 - (Absolute Difference in Market Cap / Larger Market Cap).
+Dividend Similarity Score: Again, you'll need to define what counts as a "similar" dividend yield. For instance, you might say 
+that two stocks are similar if their dividend yields are within 1% of each other. To calculate the score:
+
+If the dividend yields are within 20% of each other, the score is 1.
+If one stock's dividend yield is more than double the other's, the score is 0.
+For anything in between, you could calculate the score as 1 - (Absolute Difference in Dividend Yield / Larger Dividend Yield).
+Then, you can add up the scores for each criterion, weighted according to their importance in your analysis, to get the total 
+Similarity Score. If the Similarity Score is above a certain threshold, you might consider the stocks to be "similar".
 '''
 
 GetFacebookInformation = yahooFinance.Ticker("META")
